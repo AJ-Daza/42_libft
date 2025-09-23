116bb9874564369a0908f7b988bac4fcaf4ea220
AJDaza <223732685+AJ-Daza@users.noreply.github.com>
2025-09-23 23:55:14 +0200

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaza-ru <adaza-ru@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 13:24:43 by adaza-ru          #+#    #+#             */
+/*   Updated: 2025/04/01 13:24:45 by adaza-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*p1;
+	unsigned char	*p2;
+
+	i = 0;
+	p1 = (void *)s1;
+	p2 = (void *)s2;
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
+}
